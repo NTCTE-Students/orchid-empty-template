67a2c00555cd110feb55554487e3c0617e36db6e
paimushin.kirill <kiruha1183@gmail.com>
2024-06-03 12:47:56 +0500

    сохранение

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,49 +1,83 @@
 <?php
+    Schema::create('users', function
+(Blueprint $table) {
+    $table->id();
+    $table->string('username')->unique();
+    $table->string('password');
+    $table->enum('role', ['admin', 'manager', 'employee']);
+    $table->timestamps();
+});
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    Schema::create('clients', function
+(Blueprint $table) {
+    $table->id();
+    $table->string('name');
+    $table->string('email')->unique();
+    $table->string('phone')->nullable();
+    $table->string('address')->nullable();
+    $table->date('last_contact_date')->nullable();
+    $table->timestamps();
+});
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+    Schema::create('orders', function
+(Blueprint $table) {
+    $table->id();
+    $table->foreignId('client_id')->constrained();
+    $table->date('order_date');
+    $table->enum('status', ['pending', 'processed', 'shipped', 'delivired', 'canceled']);
+    $table->decimal('total', 10, 2);
+    $table->timestamps();
+});
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
+    Schema::create('inventory', function
+(Blueprint $table) {
+    $table->id();
+    $table->strign('item_name');
+    $table->text('description')->nullable();
+    $table->integer('quantity');
+    $table->decimal('price', 10, 2);
+    $table->integer('alert_threshold');
+    $table->timestamps();
+});
 
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
-    }
+    Schema::create('order_itmes', function
+(Blueprint $table) {
+    $table->id();
+    $table->foreignId('order_id')->constrained;
+    $table->foreignId('item_id')->constrained('inventory');
+    $table->integer('quantity');
+    $table->timestamps();
+});
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
-    }
-};
+    Schema::create('inventory', function
+(Blueprint $table) {
+    $table->id();
+    $table->foreignId('user_id')->constrained();
+    $table->foreignId('order_id')->nullable()->constrained();
+    $table->date('transaction_date');
+    $table->enum('type', ['income', 'expense']);
+    $table->decimal('price', 10, 2);
+    $table->text('description')->nullable();
+    $table->timestamps();
+});
+
+    Schema::create('notifications', function
+(Blueprint $table) {
+    $table->id();
+    $table->foreignId('user_id')->constrained();
+    $table->foreignId('client_id')->nullable()->constrained();
+    $table->foreignId('order_id')->nullable()->constrained();
+    $table->text('message');
+    $table->timestamps('sent_at')->useCurrent();
+});
+
+    Schema::create('email_logs', function
+(Blueprint $table) {
+    $table->id();
+    $table->foreignId('user_id')->constrained();
+    $table->foreignId('client_id')->nullable()->constrained();
+    $table->string('subject');
+    $table->text('body');
+    $table->timestamps('sent_at')->useCurrent();
+    $table->timestamps();
+});
